2944d2fd6d9211d17a50dd988e6d705d65376c40
apeli23 <apelibrian@gmail.com>
2022-06-14 03:05:14 +0300

    buttons

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,12 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <section>
+      <img src="stars.png" id="stars"  />
       <img src="desert.jpg" id="moon"  />
       <img src="wolf.png" id="wolf"  />
       <h2 id="text">Anime<span>JS</span></h2>
       <a href="#" id="btn">Play</a>
       <img src="cave.png" id="cave"  />
-      {/* <img src="mountains_front.png" id="mountains_front"  /> */}
     </section>
   )
 }
